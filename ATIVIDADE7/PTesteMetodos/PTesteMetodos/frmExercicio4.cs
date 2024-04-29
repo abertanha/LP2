00a5b4f44d6d7f44f34490880f7ad92c6c56ad0f
Adriano bertanha <adriano.bertanha@fatec.sp.gov.br>
2024-04-29 13:24:30 -0300

    Correção Exercício 4

--- ATIVIDADE7/PTesteMetodos/PTesteMetodos/frmExercicio4.cs
+++ ATIVIDADE7/PTesteMetodos/PTesteMetodos/frmExercicio4.cs
@@ -19,16 +19,51 @@ namespace PTesteMetodos
 
         private void btnNumero_Click(object sender, EventArgs e)
         {
-            int length = rtbTexto.Text.Length, cont = 0;
+            int length = rtbTexto.Text.Length, cont = 0, i = 0,cont2 = 0;
             char[] vetor =  rtbTexto.Text.ToCharArray();
 
-            while (length > 0) 
+            while (i < length) 
             {
-                if (char.IsNumber(vetor[length]))
+                if (char.IsNumber(vetor[i]))
                     cont++;
-
-                length--;
+                if (char.IsLetter(vetor[i]))
+                    cont2++;
+                i++;
             }
+            MessageBox.Show("Temos" + " " + cont.ToString() + " " + "numeros");
+        }
+
+        private void rtbTexto_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAlfa_Click(object sender, EventArgs e)
+        {
+            int length = rtbTexto.Text.Length, cont = 0, i = 0;
+            char[] vetor = rtbTexto.Text.ToCharArray();
+
+            while (i < length)
+            {
+                if (char.IsLetter(vetor[i]))
+                    cont++;
+                i++;
+            }
+            MessageBox.Show("Temos" + " " + cont.ToString() + " " + "letras");
+        }
+
+        private void btnBranco_Click(object sender, EventArgs e)
+        {
+            int length = rtbTexto.Text.Length, cont;
+            char[] vetor = rtbTexto.Text.ToCharArray();
+
+            for (int i = 0; i < length; i++)
+                if (char.IsWhiteSpace(vetor[i]))
+                {
+                    cont = i + 1;
+                    MessageBox.Show("O caracter espaço branco se encontra na " + " " + cont.ToString() + "° " + "posição");
+            }
+            cont = 0;
         }
     }
 }
