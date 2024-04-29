5e2aed30a735eee19fd28bff3653a9e60ff3eb77
Adriano bertanha <adriano.bertanha@fatec.sp.gov.br>
2024-04-29 13:21:48 -0300

    Correção do Exercicio 5

--- ATIVIDADE7/PTesteMetodos/PTesteMetodos/frmExercicio5.cs
+++ ATIVIDADE7/PTesteMetodos/PTesteMetodos/frmExercicio5.cs
@@ -12,9 +12,42 @@ namespace PTesteMetodos
 {
     public partial class frmExercicio5 : Form
     {
+        int num1, num2;
+
         public frmExercicio5()
         {
             InitializeComponent();
+        }
+
+        private void txtNum2_Validated(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtNum2.Text, out num2))
+            {
+                MessageBox.Show("Número inválido");
+                txtNum2.Focus();
+            }
+            if (num2 < num1)
+            {
+                MessageBox.Show("Segundo valor precisa ser maior que o primeiro");
+                txtNum2.Focus();
+            }
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            System.Random numRnd = new System.Random();
+            double num = numRnd.Next(num1, num2);
+            MessageBox.Show(num.ToString());
+
+        }
+
+        private void txtNum1_Validated(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtNum1.Text, out num1))
+            {
+                MessageBox.Show("Número inválido");
+                txtNum1.Focus();
+            }
         }
     }
 }
