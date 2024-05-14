87784a18821473257879b863474410dc8b70b240
abertanha <110491311+abertanha@users.noreply.github.com>
2024-05-14 10:46:53 -0300

    Add files via upload

---
+++ Prova/PFILME/PFILME/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace PFILME
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.lbOpiniao = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecutar.Location = new System.Drawing.Point(48, 25);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(107, 39);
+            this.btnExecutar.TabIndex = 0;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(48, 396);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(107, 33);
+            this.btnLimpar.TabIndex = 1;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // lbOpiniao
+            // 
+            this.lbOpiniao.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbOpiniao.FormattingEnabled = true;
+            this.lbOpiniao.ItemHeight = 16;
+            this.lbOpiniao.Location = new System.Drawing.Point(252, 25);
+            this.lbOpiniao.Name = "lbOpiniao";
+            this.lbOpiniao.Size = new System.Drawing.Size(536, 404);
+            this.lbOpiniao.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbOpiniao);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnExecutar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.ListBox lbOpiniao;
+    }
+}
+
---
+++ Prova/PFILME/PFILME/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace PFILME
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            double soma1 = 0, soma2 = 0;
+            double[,] matriz = new double[12, 2];
+            string aux;
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    aux = Interaction.InputBox($"Digita a nota do filme: {j + 1}", $"Pessoa {i + 1}");
+
+                    if (!double.TryParse(aux, out matriz[i, j]))
+                    {
+                        MessageBox.Show("Entrada inválida");
+                        j--;
+                    }
+                    else
+                        if (!(matriz[i, j] >= 0 && matriz[i, j] <= 10))
+                    {
+                        MessageBox.Show("Entrada inválida");
+                        j--;
+                    }
+                    else
+                    {
+                        if (j % 2 == 0)
+                            soma1 += matriz[i, j];
+                        else
+                            soma2 += matriz[i, j];
+
+                        lbOpiniao.Items.Add($"Pessoa {i + 1}: Nota Filme: {j + 1} ; {matriz[i, j]}").ToString("N2");
+                    }
+                }
+            }
+            lbOpiniao.Items.Add("----------------------------------------------------------------");
+            lbOpiniao.Items.Add($"Media Filme 1: {(soma1 / 12).ToString("N2")}\n");
+            lbOpiniao.Items.Add($"Media Filme 2: {(soma2 / 12).ToString("N2")}");
+        }
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            lbOpiniao.Items.Clear();
+        }
+    }
+}
