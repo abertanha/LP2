017562b1f0f3aff3dab3f2641dcac3f054986373
abertanha <110491311+abertanha@users.noreply.github.com>
2024-06-11 09:34:53 -0300

    Add files via upload

---
+++ PROJETOFINAL/PDisastre030482321026/PDisastre030482321026/frmCidade.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDisastre030482321026
+{
+    public partial class frmCidade : Form
+    {
+        private BindingSource bnCidade = new BindingSource();
+        private bool bInclusao = false;
+        private DataSet dsCidade = new DataSet();
+
+        public frmCidade()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCidade_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Cidade Cid = new Cidade();
+                dsCidade.Tables.Add(Cid.Listar());
+                bnCidade.DataSource = dsCidade.Tables["Cidade"];
+                dgvCidade.DataSource = bnCidade;
+                bnvCidade.BindingSource = bnCidade;
+
+                txtIdCidade.DataBindings.Add("TEXT", bnCidade, "idCidade");
+                txtNome.DataBindings.Add("TEXT", bnCidade, "nome");
+                cbxUF.DataBindings.Add("SelectedItem", bnCidade, "uf");
+                mskbxPopulacao.DataBindings.Add("TEXT", bnCidade, "populacao");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            if (tbCidade.SelectedIndex == 0)
+            {
+                tbCidade.SelectTab(1);
+            }
+            bnCidade.AddNew();
+
+            txtNome.Enabled = true;
+            cbxUF.Enabled = true;
+            cbxUF.SelectedIndex = 0;
+            mskbxPopulacao.Enabled = true;
+
+            txtNome.Focus();
+            btnSalvar.Enabled = true;
+            btnAlterar.Enabled = false;
+            btnNovo.Enabled = false;
+            btnExcluir.Enabled = false;
+            btnCancelar.Enabled = true;
+            bInclusao = true;
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            if (tbCidade.SelectedIndex == 0)
+            {
+                tbCidade.SelectTab(1);
+            }
+
+
+            txtNome.Enabled = true;
+            cbxUF.Enabled = true;
+            mskbxPopulacao.Enabled = true;
+
+            txtNome.Focus();
+            btnSalvar.Enabled = true;
+            btnAlterar.Enabled = false;
+            btnNovo.Enabled = false;
+            btnExcluir.Enabled = false;
+            btnCancelar.Enabled = true;
+
+            bInclusao = false;
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            int aux;
+
+            if (txtNome.Text == "")
+            {
+                MessageBox.Show("Nome inválido!");
+            }
+            else if (!int.TryParse(mskbxPopulacao.Text, out aux) || aux <= 0)
+            {
+                MessageBox.Show("População inválida!");
+            }
+            else
+            {
+                Cidade RegCid = new Cidade();
+
+                RegCid.nome = txtNome.Text;
+                RegCid.uf = cbxUF.SelectedItem.ToString();
+                RegCid.populacao = Convert.ToInt32(mskbxPopulacao.Text);
+
+                if (bInclusao)
+                {
+                    if (RegCid.Incluir() > 0)
+                    {
+                        MessageBox.Show("Cidade adicionado com sucesso!");
+
+                        txtIdCidade.Enabled = false;
+                        txtNome.Enabled = false;
+                        cbxUF.Enabled = false;
+                        mskbxPopulacao.Enabled = false;
+                        btnSalvar.Enabled = false;
+                        btnAlterar.Enabled = true;
+                        btnNovo.Enabled = true;
+                        btnExcluir.Enabled = true;
+                        btnCancelar.Enabled = false;
+
+                        bInclusao = false;
+
+                        //recarregar o grid
+                        dsCidade.Tables.Clear();
+                        dsCidade.Tables.Add(RegCid.Listar());
+                        bnCidade.DataSource = dsCidade.Tables["Cidade"];
+                    }
+                    else
+                    {
+                        MessageBox.Show("Erro ao gravar Cidade!");
+                    }
+                }
+                else //alteração
+                {
+                    RegCid.idCidade = Convert.ToInt32(txtIdCidade.Text);
+
+                    if (RegCid.Alterar() > 0)
+                    {
+                        MessageBox.Show("Cidade alterado com sucesso!");
+
+                        txtIdCidade.Enabled = false;
+                        txtNome.Enabled = false;
+                        cbxUF.Enabled = false;
+                        mskbxPopulacao.Enabled = false;
+
+                        btnSalvar.Enabled = false;
+                        btnAlterar.Enabled = true;
+                        btnNovo.Enabled = true;
+                        btnExcluir.Enabled = true;
+                        btnCancelar.Enabled = false;
+
+                        //recarregar o grid
+                        dsCidade.Tables.Clear();
+                        dsCidade.Tables.Add(RegCid.Listar());
+                        bnCidade.DataSource = dsCidade.Tables["Cidade"];
+                    }
+                    else
+                    {
+                        MessageBox.Show("Erro ao gravar o cidade!");
+                    }
+                }
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (tbCidade.SelectedIndex == 0)
+            {
+                tbCidade.SelectTab(1);
+            }
+            if (MessageBox.Show("Confirma exclusão?", "Sim ou não", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+                Cidade RegCid = new Cidade();
+
+                RegCid.idCidade = Convert.ToInt16(txtIdCidade.Text);
+
+                if (RegCid.Excluir() > 0)
+                {
+                    MessageBox.Show("Cidade excluída com sucesso!");
+
+                    Cidade R = new Cidade();
+                    dsCidade.Tables.Clear();
+                    dsCidade.Tables.Add(R.Listar());
+                    bnCidade.DataSource = dsCidade.Tables["Cidade"];
+                }
+                else
+                {
+                    MessageBox.Show("Erro ao excluir cidade!");
+                }
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            bnCidade.CancelEdit();
+
+            txtIdCidade.Enabled = false;
+            txtNome.Enabled = false;
+            cbxUF.Enabled = false;
+            mskbxPopulacao.Enabled = false;
+
+            btnSalvar.Enabled = false;
+            btnAlterar.Enabled = true;
+            btnNovo.Enabled = true;
+            btnExcluir.Enabled = true;
+            btnCancelar.Enabled = false;
+
+            bInclusao = false;
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ PROJETOFINAL/PDisastre030482321026/PDisastre030482321026/frmTipo.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDisastre030482321026
+{
+    public partial class frmTipo : Form
+    {
+        private BindingSource bnTipo = new BindingSource();
+        private bool bInclusao = false; 
+        private DataSet dsTipo = new DataSet();
+
+        public frmTipo()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmTipo_Load(object sender, EventArgs e)
+                
+        {
+            try
+            {
+                Tipo Tip = new Tipo();
+                dsTipo.Tables.Add(Tip.Listar());
+                bnTipo.DataSource = dsTipo.Tables["Tipo"];
+                dgvTipo.DataSource = bnTipo;
+                bnvTipo.BindingSource = bnTipo;
+
+                txtIdTipo.DataBindings.Add("TEXT", bnTipo, "idTipo");
+                txtDescricao.DataBindings.Add("TEXT", bnTipo, "descricao");
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            if (tbTipo.SelectedIndex == 0)
+            {
+                tbTipo.SelectTab(1);
+            }
+            bnTipo.AddNew();
+
+            txtDescricao.Enabled = true;
+            txtDescricao.Focus();
+
+            btnSalvar.Enabled = true;
+            btnAlterar.Enabled = false;
+            btnNovo.Enabled = false;
+            btnExcluir.Enabled = false;
+            btnCancelar.Enabled = true;
+
+            bInclusao = true;
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            if (tbTipo.SelectedIndex == 0)
+            {
+                tbTipo.SelectTab(1);
+            }
+            
+
+            txtDescricao.Enabled = true;
+            txtDescricao.Focus();
+
+            btnSalvar.Enabled = true;
+            btnAlterar.Enabled = false;
+            btnNovo.Enabled = false;
+            btnExcluir.Enabled = false;
+            btnCancelar.Enabled = true;
+
+            bInclusao = false;
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if(txtDescricao.Text == "")
+            {
+                MessageBox.Show("Descrição inválida!");
+            }
+            else 
+            {
+                Tipo RegTip = new Tipo();
+
+                RegTip.descricao = txtDescricao.Text;
+
+                if (bInclusao)
+                {
+                    if (RegTip.Incluir() > 0)
+                    {
+                        MessageBox.Show("Tipo adicionado com sucesso!");
+
+                        txtDescricao.Enabled = false;
+                        btnSalvar.Enabled = false;
+                        btnAlterar.Enabled = true;
+                        btnNovo.Enabled = true;
+                        btnExcluir.Enabled = true;
+                        btnCancelar.Enabled = false;
+
+                        bInclusao = false;
+                        //recarregar o grid
+                        dsTipo.Tables.Clear();
+                        dsTipo.Tables.Add(RegTip.Listar());
+                        bnTipo.DataSource = dsTipo.Tables["Tipo"];
+                    }
+                    else
+                    {
+                        MessageBox.Show("Erro ao gravar tipo!");
+                    }
+                }
+                else //alteração
+                {
+                    RegTip.idTipo = Convert.ToInt32(txtIdTipo.Text);
+
+                    if (RegTip.Alterar() > 0)
+                    {
+                        MessageBox.Show("Tipo alterado com sucesso!");
+
+                        dsTipo.Tables.Clear();
+                        dsTipo.Tables.Add(RegTip.Listar());
+                        bnTipo.DataSource = dsTipo.Tables["Tipo"];
+
+                        txtDescricao.Enabled = false;
+                        btnSalvar.Enabled = false;
+                        btnAlterar.Enabled = true;
+                        btnNovo.Enabled = true;
+                        btnExcluir.Enabled = true;
+                        btnCancelar.Enabled = false;
+                    }
+                    else 
+                    {
+                        MessageBox.Show("Erro ao gravar o tipo!");
+                    }
+                }
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if(tbTipo.SelectedIndex == 0)
+            {
+                tbTipo.SelectTab(1);
+            }
+            if (MessageBox.Show("Confirma exclusão?","Sim ou não",MessageBoxButtons.YesNo, MessageBoxIcon.Question,MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+                Tipo RegTip = new Tipo();
+
+                RegTip.idTipo = Convert.ToInt16(txtIdTipo.Text);
+
+                if(RegTip.Excluir () > 0) 
+                {
+                    MessageBox.Show("Tipo excluído com sucesso!");
+
+                    Tipo R = new Tipo();
+                    dsTipo.Tables.Clear();
+                    dsTipo.Tables.Add(R.Listar());
+                    bnTipo.DataSource = dsTipo.Tables["Tipo"];
+                }
+                else
+                {
+                    MessageBox.Show("Erro ao excluir tipo!");
+                }
+            }
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            bnTipo.CancelEdit();
+
+            txtDescricao.Enabled = false;
+            btnSalvar.Enabled = false;
+            btnAlterar.Enabled = true;
+            btnNovo.Enabled = true;
+            btnExcluir.Enabled = true;
+            btnCancelar.Enabled = false;
+
+            bInclusao = false;
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
